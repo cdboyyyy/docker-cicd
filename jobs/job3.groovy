774944266c5932edfee87861825898dedf8df89e
Eran Sidi <EranS@radware.com>
2021-07-13 14:15:09 +0300

    adding jobdsl

---
+++ jobs/job3.groovy
@@ -0,0 +1,24 @@
+pipelineJob('NodeJS Docker example') {
+    scm {
+        git('https://github.com/cdboyyyy/docker-cicd.git' ,'advanced') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL cdboyyyy')
+            node / gitConfigEmail('cdboyyyy@gmail.com')
+
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('cdboyyyy/course')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('34a5d27b-a837-4dd4-a52f-bc3d6748f58c')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+            buildContext('./apps/app1/')
+        }
+    }
+}
