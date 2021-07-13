30ad71aa709f69486459840b4a225b2a08e81b87
Eran Sidi <EranS@radware.com>
2021-07-13 13:57:36 +0300

    adding jobdsl

--- jobs/pipeline1.groovy
+++ jobs/pipeline1.groovy
@@ -25,7 +25,7 @@ node {
    }                                     
    stage('docker build/push') {            
      docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
-       def app = docker.build("cdboyyyy/course/docker-nodejs-demo:${commit_id}", '.').push()
+       def app = docker.build("cdboyyyy/course:${commit_id}", '.').push()
      }                                     
    }                                       
 } 
